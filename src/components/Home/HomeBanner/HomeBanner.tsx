054b6f171388b12def44a2879de8620118263b17
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-12-19 22:27:24 +0600

    Home banner  🛫

--- src/components/Home/HomeBanner/HomeBanner.tsx
+++ src/components/Home/HomeBanner/HomeBanner.tsx
@@ -3,7 +3,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper/modules";
-// home banner
+// Home banner
 import "./Banner.css";
 
 const HomeBanner = () => {
